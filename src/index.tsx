a78321a5b9d8dc701e7d8be6de6be4e9f5af587b
ikaverin <kaverin1990@gmail.com>
2022-06-25 03:19:10 +0300

    remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,5 @@ import App from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 );
